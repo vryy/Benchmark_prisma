9ee55f6ae9ea2e8994a324379aea9f9076e311cd
Hoang-Giang Bui <hgbk2008@gmail.com>
2026-08-28 18:41:17 +0100

    Add tests for axisymmetric element

---
+++ structural_application/test_axisymmetric/kinematic_linear/2d/mesh2.gid/mesh2.py
@@ -0,0 +1,142 @@
+##################################################################
+import sys
+import os
+import math
+import time as time_module
+##################################################################
+current_dir_ = os.path.dirname(os.path.realpath(__file__))
+if current_dir_ not in sys.path:
+    sys.path.append(current_dir_)
+import mesh2_include as simulation_include
+try:
+    from mesh2_include import *
+    all_modules_are_imported_successfully = True
+except Exception as e:
+    all_modules_are_imported_successfully = False
+##################################################################
+model_name_ = 'mesh2'
+##################################################################
+###  SIMULATION  #################################################
+start_time = time_module.time()
+##################################################################
+
+def WriteLog(ifile, disp, nodes):
+    reac = 0.0
+    for node in nodes:
+        reac += node.GetSolutionStepValue(REACTION_Y)
+    ifile.write("%.10e\t%.15e\n" % (disp, reac))
+    ifile.flush()
+
+def main(logging=True, output=True):
+    model = simulation_include.Model(model_name_,os.getcwd()+"/",os.getcwd()+"/",logging=logging)
+    model.InitializeModel()
+
+    ## boundary condition
+    ymin = 0.0
+    ymax = 2.667000E+01
+    xmin = 0.0
+    xmax = 6.413
+    tol = 1.0e-6
+
+    prescribed_nodes = []
+
+    for node in model.model_part.Nodes:
+        if abs(node.X0 - xmin) < tol:
+            node.Fix(DISPLACEMENT_X)
+
+        if abs(node.Y0 - ymin) < tol:
+            node.Fix(DISPLACEMENT_Y)
+
+        if abs(node.Y0 - ymax) < tol:
+            node.Fix(DISPLACEMENT_Y)
+            prescribed_nodes.append(node)
+
+    model.prescribed_nodes = prescribed_nodes
+
+    # print("prescribed_nodes:")
+    # for node in prescribed_nodes:
+    #     print(node.Id)
+    # sys.exit(0)
+
+    if logging:
+        ifile = open("monitoring.log", "w")
+        ifile.write("disp\treaction\n")
+
+    ## load increment - displacement control
+
+    time = 0.0
+    disp = 0.0
+    model.SolveModel(time)
+    if output:
+        model.WriteOutput(time)
+    if logging:
+        WriteLog(ifile, disp, prescribed_nodes)
+
+    delta_disp_list = [0.5]
+
+    print("*********LOADING STARTED**********")
+
+    for du in delta_disp_list:
+        disp += du
+        print("*********LOAD STEP " + str(disp) + " STARTED")
+        for node in prescribed_nodes:
+            node.SetSolutionStepValue(PRESCRIBED_DELTA_DISPLACEMENT_Y, du)
+            # node.SetSolutionStepValue(DISPLACEMENT_Y, disp)
+
+        delta_time = du
+        time = time + delta_time
+        model.SolveModel(time)
+        if output:
+            model.WriteOutput(time)
+        if logging:
+            WriteLog(ifile, disp, prescribed_nodes)
+
+        print("Displacement")
+        for node in model.model_part.Nodes:
+            print("%d  %.16e   %.16e" % (node.Id, node.GetSolutionStepValue(DISPLACEMENT_X), node.GetSolutionStepValue(DISPLACEMENT_Y)))
+
+    if logging:
+        ifile.close()
+    print("Analysis completed")
+
+    return model
+
+def test():
+    model = main(output=False, logging=False)
+
+    ######### pytesting results #########
+    ref_reac = 4.9623888394727197e+02
+    reac = 0.0
+    for node in model.prescribed_nodes:
+        reac += node.GetSolutionStepValue(REACTION_Y)
+    print("reac: %.16e" % (reac))
+    assert(abs(reac - ref_reac) / abs(ref_reac) < 1e-10)
+    #####################################
+    print("Test passed")
+
+def tag():
+    tags = "fbar"
+    try:
+        test_enabled = all_modules_are_imported_successfully
+    except Exception as e:
+        test_enabled = False
+    if not test_enabled:
+        tags += ",untested"
+    return tags
+
+def print_tag():
+    print("Tag(s): " + tag())
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        globals()[sys.argv[1]]() # allow to run test externally by python name.py test
+    else:
+        main(logging=True, output=True)
+
+##################################################################
+###  END OF SIMULATION  ##########################################
+end_time = time_module.time()
+print("Calculation time: " + str(end_time - start_time) + " s")
+timer = Timer()
+print(timer)
+##################################################################
---
+++ structural_application/test_axisymmetric/kinematic_linear/3d/mesh2.gid/mesh2.py
@@ -0,0 +1,149 @@
+##################################################################
+import sys
+import os
+import math
+import time as time_module
+##################################################################
+current_dir_ = os.path.dirname(os.path.realpath(__file__))
+if current_dir_ not in sys.path:
+    sys.path.append(current_dir_)
+import mesh2_include as simulation_include
+try:
+    from mesh2_include import *
+    all_modules_are_imported_successfully = True
+except Exception as e:
+    all_modules_are_imported_successfully = False
+##################################################################
+model_name_ = 'mesh2'
+##################################################################
+###  SIMULATION  #################################################
+start_time = time_module.time()
+##################################################################
+
+def WriteLog(ifile, disp, nodes):
+    reac = 0.0
+    for node in nodes:
+        reac += node.GetSolutionStepValue(REACTION_Y)*4
+    ifile.write("%.10e\t%.15e\n" % (disp, reac))
+    ifile.flush()
+
+def main(output=True, logging=True, dry_run=False):
+
+    model = simulation_include.Model(model_name_,current_dir_,current_dir_,logging=logging)
+    model.InitializeModel()
+
+    ## boundary condition
+    ymin = 0.0
+    ymax = 2.667000E+01
+    xmin = 0.0
+    xmax = 6.413
+    zmin = 0.0
+    tol = 1.0e-6
+
+    prescribed_nodes = []
+
+    for node in model.model_part.Nodes:
+        if abs(node.X0 - xmin) < tol:
+            node.Fix(DISPLACEMENT_X)
+
+        if abs(node.Z0 - zmin) < tol:
+            node.Fix(DISPLACEMENT_Z)
+
+        if abs(node.Y0 - ymin) < tol:
+            node.Fix(DISPLACEMENT_Y)
+
+        if abs(node.Y0 - ymax) < tol:
+            node.Fix(DISPLACEMENT_Y)
+            prescribed_nodes.append(node)
+
+    model.prescribed_nodes = prescribed_nodes
+
+    # print("prescribed_nodes:")
+    # for node in prescribed_nodes:
+    #     print(node.Id)
+    # sys.exit(0)
+
+    if logging:
+        ifile = open("monitoring.log", "w")
+        ifile.write("disp\treaction\n")
+
+    ## load increment - displacement control
+
+    time = 0.0
+    disp = 0.0
+    model.SolveModel(time)
+    if output:
+        model.WriteOutput(time)
+    if logging:
+        WriteLog(ifile, disp, prescribed_nodes)
+
+    delta_disp_list = [0.5]
+
+    print("*********LOADING STARTED**********")
+
+    for du in delta_disp_list:
+        disp += du
+        print("*********LOAD STEP " + str(disp) + " STARTED")
+        for node in prescribed_nodes:
+            node.SetSolutionStepValue(PRESCRIBED_DELTA_DISPLACEMENT_Y, du)
+            # node.SetSolutionStepValue(DISPLACEMENT_Y, disp)
+
+        delta_time = du
+        time = time + delta_time
+        if not dry_run:
+            model.SolveModel(time)
+        if output:
+            model.WriteOutput(time)
+        if logging:
+            WriteLog(ifile, disp, prescribed_nodes)
+
+        # print("Displacement")
+        # for node in model.model_part.Nodes:
+        #     print("%d  %.16e   %.16e" % (node.Id, node.GetSolutionStepValue(DISPLACEMENT_X), node.GetSolutionStepValue(DISPLACEMENT_Y)))
+
+    if logging:
+        ifile.close()
+
+    print("Analysis completed")
+
+    return model
+
+def test():
+    model = main(output=False, logging=False)
+
+    ######### pytesting results #########
+    ref_reac = 4.9305645050425534e+02
+    reac = 0.0
+    for node in model.prescribed_nodes:
+        reac += node.GetSolutionStepValue(REACTION_Y)*4
+    print("reac: %.16e" % (reac))
+    assert(abs(reac - ref_reac) / abs(ref_reac) < 1e-10)
+    #####################################
+    print("Test passed")
+
+def tag():
+    tags = "fbar"
+    try:
+        test_enabled = all_modules_are_imported_successfully
+    except Exception as e:
+        test_enabled = False
+    if not test_enabled:
+        tags += ",untested"
+    return tags
+
+def print_tag():
+    print("Tag(s): " + tag())
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        globals()[sys.argv[1]]() # allow to run test externally by python name.py test
+    else:
+        main(logging=True, output=True)
+
+##################################################################
+###  END OF SIMULATION  ##########################################
+end_time = time_module.time()
+print("Calculation time: " + str(end_time - start_time) + " s")
+timer = Timer()
+print(timer)
+##################################################################
