04f90fb6542d027e69d09d1ecf842f3210663120
Hoang-Giang Bui <hgbk2008@gmail.com>
2026-08-28 19:12:36 +0100

    Add test for 3d plasticity POD

---
+++ ersatz_anwendung/structural_application/test_multiplicative_finite_strain_bridging/example_14_9_2/tresca/mesh_h8_fbar_tresca_dc_ecsw/mesh2.gid/mesh2.py
@@ -0,0 +1,257 @@
+##################################################################
+import sys
+import os
+import math
+import time as time_module
+##################################################################
+current_dir_ = os.path.dirname(os.path.realpath(__file__))
+if current_dir_ not in sys.path:
+    sys.path.append(current_dir_)
+import mesh2_include as simulation_include
+try:
+    from mesh2_include import *
+    all_modules_are_imported_successfully = True
+except Exception as e:
+    all_modules_are_imported_successfully = False
+##################################################################
+model_name_ = 'mesh2'
+##################################################################
+###  SIMULATION  #################################################
+start_time = time_module.time()
+##################################################################
+
+def WriteLog(ifile, disp, nodes):
+    reac = 0.0
+    for node in nodes:
+        reac += node.GetSolutionStepValue(REACTION_Y)*4
+    ifile.write("%.10e\t%.15e\n" % (disp, reac))
+    ifile.flush()
+
+def main(output=True, logging=True, pod="load", number_of_pod_modes=12, dry_run=False, check=False):
+
+    model = simulation_include.Model(model_name_,current_dir_,current_dir_,logging=logging)
+    model.InitializeModel()
+
+    pod_utils = POD_Utils()
+
+    if pod == "save":
+        pod_process = EcswSnapshotCollectingProcess(model.solver.solver.linear_solver)
+        pod_process.SetForceTolerance(1.0)
+        pod_process.SetNormalize(False)
+        model.solver.solver.builder_and_solver.SetPodProcess(pod_process)
+        model.solver.solver.linear_solver = pod_process
+    elif pod == "load":
+        # POD without hyper reduction
+        Phi = pod_utils.ReadMat("Gb.mat", 'Phi')
+        pod_process = RayleighRitzProjectionProcess(Phi)
+        model.solver.solver.builder_and_solver.SetPodProcess(pod_process)
+        decouple_build_and_solve = model.analysis_parameters['decouple_build_and_solve']
+        if decouple_build_and_solve:
+            model.solver.solver.linear_solver = pod_process
+            # this must be set if decouple_build_and_solve == True
+            # since pod_process is also a linear solver, this is usable and necessary
+    elif pod == "load-ecsw":
+        # POD with hyper reduction
+        Phi = pod_utils.ReadMat("Gb.mat", 'Phi')
+        eid = pod_utils.ReadIntVec("Gb.mat", 'eid')
+        ewi = pod_utils.ReadIntVec("Gb.mat", 'ewi')
+        pod_utils.ListVariables("ew.mat")
+        w = pod_utils.ReadVec("ew.mat", 'x')
+        pod_process = RayleighRitzProjectionProcess(Phi)
+        model.solver.solver.time_scheme = ElementWeightingScheme(model.solver.solver.time_scheme)
+        for i in range(len(eid)):
+            model.solver.solver.time_scheme.SetElementWeight(eid[i], w[ewi[i]])
+        if check:
+            model.solver.solver.time_scheme.SetProjectionOperator(Phi)
+        model.solver.solver.builder_and_solver.SetPodProcess(pod_process)
+        decouple_build_and_solve = model.analysis_parameters['decouple_build_and_solve']
+        if decouple_build_and_solve:
+            model.solver.solver.linear_solver = pod_process
+            # this must be set if decouple_build_and_solve == True
+            # since pod_process is also a linear solver, this is usable and necessary
+
+    ## boundary condition
+    ymin = 0.0
+    ymax = 2.667000E+01
+    xmin = 0.0
+    xmax = 6.413
+    zmin = 0.0
+    tol = 1.0e-6
+
+    prescribed_nodes = []
+
+    for node in model.model_part.Nodes:
+        if abs(node.X0 - xmin) < tol:
+            node.Fix(DISPLACEMENT_X)
+
+        if abs(node.Z0 - zmin) < tol:
+            node.Fix(DISPLACEMENT_Z)
+
+        if abs(node.Y0 - ymin) < tol:
+            node.Fix(DISPLACEMENT_Y)
+
+        if abs(node.Y0 - ymax) < tol:
+            node.Fix(DISPLACEMENT_Y)
+            prescribed_nodes.append(node)
+
+    model.prescribed_nodes = prescribed_nodes
+
+    # print("prescribed_nodes:")
+    # for node in prescribed_nodes:
+    #     print(node.Id)
+    # sys.exit(0)
+
+    if logging:
+        ifile = open("monitoring.log", "w")
+        ifile.write("disp\treaction\n")
+
+    ## load increment - displacement control
+
+    time = 0.0
+    disp = 0.0
+    model.SolveModel(time)
+    if output:
+        model.WriteOutput(time)
+    if logging:
+        WriteLog(ifile, disp, prescribed_nodes)
+
+    delta_disp_list = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.125, 0.125, 0.25, 0.5]   # 4
+    delta_disp_list.extend([0.25]*4)        # 5
+    delta_disp_list.extend([0.125]*7)
+    delta_disp_list.extend([0.125/2]*2)     # 6
+    delta_disp_list.extend([0.125/2]*13)
+    delta_disp_list.extend([0.125/4]*6)     # 7
+
+    print("*********LOADING STARTED**********")
+
+    for du in delta_disp_list:
+        disp += du
+        print("*********LOAD STEP " + str(disp) + " STARTED")
+        for node in prescribed_nodes:
+            node.SetSolutionStepValue(PRESCRIBED_DELTA_DISPLACEMENT_Y, du)
+            # node.SetSolutionStepValue(DISPLACEMENT_Y, disp)
+
+        delta_time = du
+        time = time + delta_time
+        if not dry_run:
+            model.SolveModel(time)
+        if output:
+            model.WriteOutput(time)
+        if logging:
+            WriteLog(ifile, disp, prescribed_nodes)
+
+        # print("Displacement")
+        # for node in model.model_part.Nodes:
+        #     print("%d  %.16e   %.16e" % (node.Id, node.GetSolutionStepValue(DISPLACEMENT_X), node.GetSolutionStepValue(DISPLACEMENT_Y)))
+
+    if logging:
+        ifile.close()
+
+    if pod == "save":
+        # S = model.solver.solver.builder_and_solver.GetPodProcess().GetPrincipalValues()
+        # print(S)
+        # sys.exit(0)
+        Phi = model.solver.solver.builder_and_solver.GetPodProcess().GetPrincipalComponents(number_of_pod_modes)
+        G = Matrix(1, 1)
+        b = Vector(1)
+        windex = model.solver.solver.builder_and_solver.GetPodProcess().ConstructSystem(G, b, number_of_pod_modes)
+        # print(windex)
+        eid = IntegerVector(len(windex))
+        ewi = IntegerVector(len(windex))
+        cnt = 0
+        for k, i in windex.items():
+            eid[cnt] = k
+            ewi[cnt] = i
+            cnt += 1
+        # print(f"eid: {eid}")
+        # print(f"ewi: {ewi}")
+        pod_utils.WriteMat("Gb.mat", "G", G, False)
+        pod_utils.WriteVec("Gb.mat", "b", b, True)
+        pod_utils.WriteMat("Gb.mat", "Phi", Phi, True)
+        pod_utils.WriteIntVec("Gb.mat", "eid", eid, True)
+        pod_utils.WriteIntVec("Gb.mat", "ewi", ewi, True)
+
+        w = Vector(1)
+        LapackNnlsSolver.Solve(G, w, b, 'QR', 100, -1.0)
+        pod_utils.WriteVec("ew.mat", "x", w, False)
+
+    print("Analysis completed")
+
+    return model
+
+def test1():
+    model = main(output=False, logging=False, pod = "save")
+
+    ######### pytesting results #########
+    ref_reac = 2.0759262156662025e+01
+    reac = 0.0
+    for node in model.prescribed_nodes:
+        reac += node.GetSolutionStepValue(REACTION_Y)*4
+    print("reac: %.16e" % (reac))
+    assert(abs(reac - ref_reac) / abs(ref_reac) < 1e-10)
+    #####################################
+    print("Test 1 passed")
+    # 113.52138185501099 s
+
+def test2():
+    model = main(output=False, logging=False, pod = "load", dry_run=False, check=False)
+
+    ######### pytesting results #########
+    ref_reac = 2.1281790096153781e+01
+    reac = 0.0
+    for node in model.prescribed_nodes:
+        reac += node.GetSolutionStepValue(REACTION_Y)*4
+    print("reac: %.16e" % (reac))
+    assert(abs(reac - ref_reac) / abs(ref_reac) < 1e-10)
+    #####################################
+    print("Test 2 passed")
+    #  46.44733119010925 s
+
+def test3():
+    model = main(output=False, logging=True, pod = "load-ecsw", dry_run=False, check=False)
+
+    ######### pytesting results #########
+    ref_reac = 2.1274770851516042e+01
+    reac = 0.0
+    for node in model.prescribed_nodes:
+        reac += node.GetSolutionStepValue(REACTION_Y)*4
+    print("reac: %.16e" % (reac))
+    assert(abs(reac - ref_reac) / abs(ref_reac) < 1e-10)
+    #####################################
+    print("Test 3 passed")
+    # 43.60981249809265 s
+
+def test():
+    test1()
+    test2()
+    test3()
+    print("All local tests passed")
+
+def tag():
+    tags = "pod,ecsw,plastic"
+    try:
+        test_enabled = all_modules_are_imported_successfully
+        test_enabled = test_enabled and KratosErsatzAnwendung.Has('Matio')
+        test_enabled = test_enabled and hasattr(KratosMultiphysics, "LapackNnlsSolver")
+    except Exception as e:
+        test_enabled = False
+    if not test_enabled:
+        tags += ",untested"
+    return tags
+
+def print_tag():
+    print("Tag(s): " + tag())
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        globals()[sys.argv[1]]() # allow to run test externally by python name.py test
+    else:
+        main(logging=True, output=True)
+
+##################################################################
+###  END OF SIMULATION  ##########################################
+end_time = time_module.time()
+print("Calculation time: " + str(end_time - start_time) + " s")
+timer = Timer()
+print(timer)
+##################################################################
---
+++ ersatz_anwendung/structural_application/test_multiplicative_finite_strain_bridging/example_14_9_2/tresca/mesh_h8_fbar_tresca_dc_ecsw/mesh2.gid/mesh2_include.py
@@ -0,0 +1,313 @@
+##################################################################
+import sys
+import os
+import six # for iteritems
+import time as time_module
+##################################################################
+##################################################################
+#importing Kratos modules
+from KratosMultiphysics import *
+from KratosMultiphysics.StructuralApplication import *
+from KratosMultiphysics.SoilMechanicsApplication import *
+from KratosMultiphysics.ExternalSolversApplication import *
+from KratosMultiphysics.MKLSolversApplication import *
+from KratosMultiphysics.BRepApplication import *
+from KratosMultiphysics.LayerApplication import *
+from KratosMultiphysics.ErsatzAnwendung import *
+kernel = Kernel()   #defining kernel
+##################################################################
+##################################################################
+class Model:
+    def __init__( self, problem_name, path, results_path, logging=True ):
+        #setting the domain size for the problem to be solved
+        ##################################################################
+        ## DEFINE MODELPART ##############################################
+        ##################################################################
+        self.model_part = ModelPart(problem_name)
+        self.path = path+os.sep
+        self.results_path = results_path+os.sep
+        self.problem_name = problem_name
+        ##################################################################
+        ## DEFINE SOLVER #################################################
+        ##################################################################
+        # reading simulation parameters
+        self.analysis_parameters = {}
+        # analysis type: static (0), quasi-static (1) or dynamic (2)
+        self.analysis_parameters['print_sparsity_info_flag'] = False
+        self.analysis_parameters['analysis_type'] = 0
+        self.analysis_parameters['dissipation_radius'] =          0.1
+        self.analysis_parameters['decouple_build_and_solve'] = True
+        self.analysis_parameters['solving_scheme'] = 'monolithic'
+        self.analysis_parameters['stop_Newton_Raphson_if_not_converge'] = True
+        self.analysis_parameters['number_of_iterations_for_divergence_check'] = 10
+        self.analysis_parameters['list_dof'] = True
+        self.analysis_parameters['log_residuum'] = logging
+
+        self.abs_tol =        1e-10
+        self.rel_tol =        1e-10
+
+        ## generating solver
+        import structural_solver_advanced
+        self.solver = structural_solver_advanced.SolverAdvanced( self.model_part, self.analysis_parameters, self.abs_tol, self.rel_tol )
+        self.AddVariables( self.model_part )
+        ##################################################################
+        ## READ MODELPART ################################################
+        ##################################################################
+        #reading a model
+        self.model_part_io = ModelPartIO(self.path+self.problem_name)
+        self.model_part_io.ReadModelPart(self.model_part)
+        self.meshWritten = False
+        (self.solver).CalculateReactionFlag = True
+        #the buffer size should be set up here after the mesh is read for the first time
+        self.model_part.SetBufferSize(2)
+
+        ##################################################################
+        ## POST_PROCESSING ###############################################
+        ##################################################################
+
+        self.write_deformed_flag = WriteDeformedMeshFlag.WriteUndeformed
+        self.write_elements = WriteConditionsFlag.WriteConditions
+        #write_elements = WriteConditionsFlag.WriteElementsOnly
+        self.post_mode = GiDPostMode.GiD_PostBinary
+        self.multi_file_flag = MultiFileFlag.MultipleFiles
+        self.gid_io = SDGidPostIO( self.results_path+self.problem_name, self.post_mode, self.multi_file_flag, self.write_deformed_flag, self.write_elements )
+
+        # define the output variables
+        self.output_nodal_variables = []
+        self.output_intpt_variables = []
+
+        self.output_nodal_variables.append(DISPLACEMENT)
+
+        self.output_intpt_variables.append(STRESSES)
+        self.output_intpt_variables.append(PLASTICITY_INDICATOR)
+        self.output_intpt_variables.append(ELEMENT_WEIGHT)
+        ##################################################################
+        ## ADD DOFS ######################################################
+        ##################################################################
+        self.AddDofs(self.model_part)
+
+        ##################################################################
+        ## INITIALISE SOLVER FOR PARTICULAR SOLUTION #####################
+        ##################################################################
+        #defining linear solver
+        if KratosMKLSolversApplication.Has("MKLPardisoSolver"):
+            plinear_solver = MKLPardisoSolver()
+        else:
+            plinear_solver = SuperLUSolver()
+        self.solver.structure_linear_solver = plinear_solver
+        self.solver.Initialize()
+        (self.solver.solver).SetEchoLevel(2)
+        (self.solver.solver).max_iter = 20 #control the maximum iterations of Newton Raphson loop
+        (self.solver.solver).MoveMeshFlag = False
+
+        # replace the builder and solver by the POD type
+        factory = ProjectionBasedPodBuilderAndSolverFactory()
+        (self.solver.solver).builder_and_solver = factory.Create((self.solver.solver).builder_and_solver)
+
+        ##################################################################
+        ## INITIALISE RESTART UTILITY ####################################
+        ##################################################################
+        #restart_utility= RestartUtility( self.problem_name )
+
+    def AddDofsForNodes(self, nodes):
+        import structural_solver_advanced
+        structural_solver_advanced.AddDofsForNodes( nodes )
+
+    def AddDofs(self, model_part):
+        import structural_solver_advanced
+        structural_solver_advanced.AddDofs( model_part )
+
+    def AddDofsForNode(self, node):
+        import structural_solver_advanced
+        structural_solver_advanced.AddDofsForNode( node )
+
+    def AddVariables(self, model_part):
+        import structural_solver_advanced
+        structural_solver_advanced.AddVariables( model_part )
+
+    def SetModelPart(self, model_part):
+        self.model_part = model_part
+
+        ## generating solver
+        import structural_solver_advanced
+        self.solver = structural_solver_advanced.SolverAdvanced( self.model_part, self.analysis_parameters, self.abs_tol, self.rel_tol )
+        (self.solver).CalculateReactionFlag = False
+        ##################################################################
+        ## ADD DOFS ######################################################
+        ##################################################################
+        self.AddDofs(self.model_part)
+
+        ##################################################################
+        ## INITIALISE SOLVER FOR PARTICULAR SOLUTION #####################
+        ##################################################################
+        #defining linear solver
+        if KratosMKLSolversApplication.Has("MKLPardisoSolver"):
+            plinear_solver = MKLPardisoSolver()
+        else:
+            plinear_solver = SuperLUSolver()
+        self.solver.structure_linear_solver = plinear_solver
+        self.solver.Initialize()
+        (self.solver.solver).SetEchoLevel(2)
+
+    def SetOutputPath(self, path):
+        self.results_path = path
+        self.gid_io = SDGidPostIO( self.results_path+self.problem_name, self.post_mode, self.multi_file_flag, self.write_deformed_flag, self.write_elements )
+
+
+    def WriteOutput( self, time ):
+        self.gid_io.InitializeMesh( time )
+        mesh = self.model_part.GetMesh()
+        print("mesh at time " + str(time) + " is ready for printing")
+        #self.gid_io.WriteNodeMesh( mesh )
+        self.gid_io.WriteMesh( mesh )
+        print("mesh written...")
+        self.gid_io.FinalizeMesh()
+        self.gid_io.InitializeResults( time, self.model_part.GetMesh() )
+        for var in self.output_nodal_variables:
+            self.gid_io.WriteNodalResults(var, time, 0)
+            print(f"nodal {var} written")
+        for var in self.output_intpt_variables:
+            self.gid_io.PrintOnGaussPoints(var, self.model_part, time)
+            print(f"gauss point {var} written")
+        self.gid_io.FinalizeResults()
+        self.gid_io.Reset()
+
+    def InitializeModel( self ):
+        ##################################################################
+        ## STORE LAYER SETS ##############################################
+        ##################################################################
+        model_layers = __import__(self.problem_name+"_layers")
+        ## ELEMENTS on layers ############################################
+        self.layer_sets = model_layers.ReadLayerSets()
+        ## NODES on layers ###############################################
+        self.layer_nodes_sets = model_layers.ReadLayerNodesSets()
+        ## CONTACT MASTER NODES ##########################################
+        #self.contact_master_nodes = model_layers.ReadContactMasterNodes()
+        ## CONTACT SLAVE NODES ###########################################
+        #self.contact_slave_nodes = model_layers.ReadContactSlaveNodes()
+        ##################################################################
+        print("layer sets stored")
+        ##################################################################
+        ## STORE NODES ON GROUND SURFACE #################################
+        ##################################################################
+        self.top_surface_nodes = model_layers.ReadTopSurfaceNodes()
+        print("nodes on ground surface stored")
+        ##################################################################
+        ## STORE NODES ON SIDE ###########################################
+        ##################################################################
+        self.boundary_nodes = model_layers.ReadBoundaryNodes()
+        print("nodes on side surface stored")
+        ##################################################################
+        ## STORE NODES CORRECTLY FOR CONDITIONS ##########################
+        ##################################################################
+        self.node_groups = model_layers.ReadNodeGroups()
+        print("node groups stored")
+        ##################################################################
+        ## EXTRACT CONDITIONS FROM NODE GROUPS ###########################
+        ##################################################################
+        start_time = time_module.time()
+        self.layer_cond_sets = {}
+        for layer, node_group in six.iteritems(self.node_groups):
+            self.layer_cond_sets[layer] = []
+        for layer, node_group in six.iteritems(self.node_groups):
+            for cond in self.model_part.Conditions:
+                in_group = True
+                for node in cond.GetNodes():
+                    if node.Id not in node_group:
+                        in_group = False
+                        break
+                if in_group:
+                    self.layer_cond_sets[layer].append(cond.Id)
+        end_time = time_module.time()
+        print("conditions in node groups stored, time = " + str(end_time - start_time) + "s")
+        ##################################################################
+        ## INITIALISE CONSTITUTIVE LAWS ##################################
+        ##################################################################
+        #set material parameters
+        self.model_part.Properties[1].SetValue(DENSITY,         0.0 )
+        self.model_part.Properties[1].SetValue(YOUNG_MODULUS,        206.9 )
+        self.model_part.Properties[1].SetValue(POISSON_RATIO,          0.29 )
+        self.model_part.Properties[1].SetValue(THICKNESS,            1 )
+        self.model_part.Properties[1].SetValue(IS_FBAR,            True )
+        self.model_part.Properties[1].SetValue(CONSTITUTIVE_LAW, MultiplicativeFiniteStrainBridgingConstitutiveLawDC_Kirchhoff(Tresca3dImplicitDC()) )
+        hlaw = PiecewiseLinearHardeningLaw()
+        coef = 1.0 #2.0/(3**0.5)
+        hlaw.AddPoint(  0.0     ,   0.45        * coef   )
+        hlaw.AddPoint(  0.001   ,   0.454577    * coef   )
+        hlaw.AddPoint(  0.002   ,   0.459081    * coef   )
+        hlaw.AddPoint(  0.005   ,   0.472155    * coef   )
+        hlaw.AddPoint(  0.01    ,   0.492564    * coef   )
+        hlaw.AddPoint(  0.02    ,   0.528701    * coef   )
+        hlaw.AddPoint(  0.03    ,   0.559411    * coef   )
+        hlaw.AddPoint(  0.04    ,   0.585539    * coef   )
+        hlaw.AddPoint(  0.05    ,   0.607799    * coef   )
+        hlaw.AddPoint(  0.06    ,   0.626794    * coef   )
+        hlaw.AddPoint(  0.07    ,   0.643032    * coef   )
+        hlaw.AddPoint(  0.08    ,   0.656942    * coef   )
+        hlaw.AddPoint(  0.09    ,   0.668887    * coef   )
+        hlaw.AddPoint(  0.1     ,   0.679172    * coef   )
+        hlaw.AddPoint(  0.12    ,   0.69576     * coef   )
+        hlaw.AddPoint(  0.14    ,   0.708326    * coef   )
+        hlaw.AddPoint(  0.16    ,   0.718025    * coef   )
+        hlaw.AddPoint(  0.2     ,   0.731879    * coef   )
+        hlaw.AddPoint(  0.25    ,   0.743463    * coef   )
+        hlaw.AddPoint(  0.3     ,   0.752122    * coef   )
+        hlaw.AddPoint(  0.5     ,   0.779564    * coef   )
+        hlaw.AddPoint(  1.0     ,   0.84424     * coef   )
+        hlaw.AddPoint(  11.0    ,   2.13664     * coef   )
+        hlaw.Assign(ISOTROPIC_HARDENING_LAW, hlaw, self.model_part.Properties[1])
+        print("Multiplicative finite strain bridging constitutive law is selected")
+        ##################################################################
+        ## ACTIVATION ####################################################
+        ##################################################################
+        self.deac = DeactivationUtility()
+        self.deac.Initialize( self.model_part )
+        self.solver.solver.Initialize()
+        self.model_part.Check( self.model_part.ProcessInfo )
+        print("activation utility initialized")
+        print("model successfully initialized")
+
+    def WriteRestartFile( self, time ):
+        fn = self.problem_name + "_" + str(time)
+        serializer = Serializer(fn)
+        serializer.Save("ModelPart", self.model_part)
+        serializer = 0
+        print("Write restart data to " + fn + ".rest completed")
+
+    def LoadRestartFile( self, time ):
+        fn = self.problem_name + "_" + str(time)
+        serializer = Serializer(fn)
+        serializer.Load("ModelPart", self.model_part)
+        serializer = 0
+        print("Load restart data from " + fn + ".rest completed")
+
+    def FinalizeModel( self ):
+        self.gid_io.CloseResultFile()
+
+    # solve with deactivation/reactivation
+    # element/condition with nonzero ACTIVATION_LEVEL in [from_deac, to_deac] will be deactivated
+    # element/condition with negative ACTIVATION_LEVEL will also be deactivated
+    # element/condition with ACTIVATION_LEVEL in [from_reac, to_reac] will be re-activated
+    def Solve( self, time, from_deac, to_deac, from_reac, to_reac ):
+        self.deac.Reactivate( self.model_part, from_reac, to_reac )
+        self.deac.Deactivate( self.model_part, from_deac, to_deac )
+        self.model_part.CloneTimeStep(time)
+        self.solver.Solve()
+
+    # solve nothing (good for debugging) with deactivation/reactivation
+    def DrySolve( self, time, from_deac, to_deac, from_reac, to_reac ):
+        self.deac.Reactivate( self.model_part, from_reac, to_reac )
+        self.deac.Deactivate( self.model_part, from_deac, to_deac )
+        self.model_part.CloneTimeStep(time)
+
+    # solve without deactivation
+    def SolveModel(self, time):
+        self.model_part.CloneTimeStep(time)
+        success = self.solver.Solve()
+        return success
+
+    # solve nothing without deactivation
+    def DrySolveModel(self, time):
+        self.model_part.CloneTimeStep(time)
+
+##################################################################
